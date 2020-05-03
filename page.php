02a92bfb9c1cc70a753ebdb69d7b6be0735953b1
creativeartbd <creativeartbd@gmail.com>
2020-05-03 20:07:54 +0600

    Style Updated

--- page.php
+++ page.php
@@ -14,25 +14,24 @@
 
 get_header();
 ?>
+<div class="container main-container">
+	<div class="row">
+		<div class="col-lg">
+			<main id="primary" class="site-main">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/content', 'page' );
 
-	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			endwhile; // End of the loop.
+			?>
+			</main><!-- #main -->
+		</div>		
+	</div>
+</div>
 <?php
-get_sidebar();
 get_footer();
