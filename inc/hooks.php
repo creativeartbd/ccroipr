04f330b0f4732e8ebd14af0d22275ee2b97d027b
creativeartbd <creativeartbd@gmail.com>
2020-05-11 15:48:26 +0600

    Upload folder permission

--- inc/hooks.php
+++ inc/hooks.php
@@ -223,7 +223,7 @@ EOD;
         $upload_dir     = $upload_dir . '/ccroipr-pdf/';
 
         if (! is_dir($upload_dir)) {
-            mkdir( $upload_dir, 0777 );
+            mkdir( $upload_dir, 0755 );
         }        
 
         $filename= $confirm_id.'.pdf';      
