c1e143b2174977a6790526a2f38c46daf2642da1
creativeartbd <creativeartbd@gmail.com>
2020-07-11 13:04:55 +0600

    Replace the current logo

--- header.php
+++ header.php
@@ -31,7 +31,7 @@ defined( 'ABSPATH' ) || exit;
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">				
-					<img src="<?php echo get_template_directory_uri() . '/assets/img/logoline-padp-art.jpg'; ?>" alt="">
+					<img src="<?php echo get_template_directory_uri() . '/assets/img/logoline-padp-ccroipr.jpg'; ?>" alt="">
 				</div>	
 			</div>
 		</div>
