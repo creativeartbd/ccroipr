4e2858261f01aaf19f33c72b57f35dce36838ce5
creativeartbd <creativeartbd@gmail.com>
2020-08-22 09:23:27 +0600

    registration confirmation page cleared commented code

--- page-templates/registration-confirmation.php
+++ page-templates/registration-confirmation.php
@@ -32,33 +32,7 @@ if( isset( $key, $post_id ) ) {
 				}
 			}			
 		}
-
 		
-		// global $wpdb;   
-
-		// $author_page 	= get_author_posts_url( $user_id );										
-		// $table_name 	= $wpdb->prefix.'users';
-		// $query 			= $wpdb->get_row( "SELECT user_status FROM $table_name WHERE ID = $user_id AND user_activation_key = '$key' " );
-
-		// if( $query ) {
-		// 	$user_status = $query->user_status;
-		// 	if( 0 == $user_status ) {
-		// 		$wpdb->update( 
-		// 	    	$table_name,
-		// 	     	array( 'user_status' => 1 ),
-		// 	     	array( 
-		// 	     		'ID' =>  $user_id, 
-		// 	       		'user_activation_key' => $key,
-		// 	     	), 
-		// 	     	array( '%d', '%s')
-		// 		);		
-		// 		$message .= "<div class='alert alert-success'>Congratulation! Your email has been confirmed.</div>";
-		// 		header( "refresh:5;url=$author_page" );
-		// 	} else {
-		// 		$message .= "<div class='alert alert-warning'>Your email address is already confirmed</div>"; 
-		// 		header( "refresh:5;url=$author_page" );
-		// 	}
-		// }
 	}						
 }
 
