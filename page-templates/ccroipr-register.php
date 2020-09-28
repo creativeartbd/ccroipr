3f6f49649aef1963dbfc0c0d8ef749242f7d582c
creativeartbd <creativeartbd@gmail.com>
2020-09-28 15:43:59 +0600

    Update form

--- page-templates/ccroipr-register.php
+++ page-templates/ccroipr-register.php
@@ -21,11 +21,13 @@ get_header();
 			}
 			?>	
 			<form action="" method="POST" enctype="multipart/form-data" id="ccroipr_ru_form">
-				<div class="row mt-5">					
-					<div class="col-sm-6 col-md-4 col-lg-4">
-						<div class="form-group">
+				<div class="row mt-5">	
+					<div class="col-md-12">
+					<div class="form-group">
                             <label for="">Urheber - Impressum nach $55 RStV</label>
                         </div>
+					</div>				
+					<div class="col-sm-6 col-md-6 col-lg-6">						
 						<div class="form-group">
 							<label for="">Surname</label>
 							<input type="text" name="surname" class="form-control" placeholder="Surname">
@@ -41,10 +43,8 @@ get_header();
 						<div class="form-group">
                             <label for="">Plz</label>
                             <input type="text" name="plz" class="form-control" maxlength="10" placeholder="Plz">
-                        </div>                                         
-					</div>
-					<div class="col-sm-6 col-md-4 col-lg-4">						
-                        <div class="form-group">
+						</div>  
+						<div class="form-group">
                             <label for="">Ort / Stadt</label>
                             <input type="text" name="ort" class="form-control" maxlength="35" placeholder="Ort / Stadt">
                         </div>
@@ -55,32 +55,29 @@ get_header();
 					 	<div class="form-group">
                             <label for="">Werktitel</label>
                             <input type="text" name="werktitel" id="werktitel" maxlength="30" class="form-control" placeholder="Werktitel">
-						</div>     
+						</div>                                            
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-6">
 						<div class="form-group">							
 							<div class="slim" data-instant-edit="true">
 								<input type="file" name="slim" id="file_change"/>
 							</div>
-	                    </div>                   					
-					</div>
-					<div class="col-sm-6 col-md-4 col-lg-4">						
+						</div> 
 						<div class="form-group">
                             <label for="">SHA256 (Hashwert der Originalabbildung)</label>
                             <input type="text" id="sha256" name="sha256" maxlength="64" class="form-control" placeholder="SHA256 (Hashwert der Originalabbildung)" readonly>
-                        </div>
+                        </div>	
                         <div class="form-group">
                             <label for="">Werk-Beschreibung</label>
                             <textarea id="limit" name="werk_beschreibung" cols="30" rows="10" class="form-control" placeholder="Werk-Beschreibung"></textarea><span class="counter"></span>
-                        </div>
-					</div>										
+						</div>                 					
+					</div>							
 				</div>				
 				<div class="row">
 					<div class="col-md-12">
-						<div class="form-group">
-	                        <p>Der Urheber ist vollständig fur den Inhalt der Darstellung verantworlich und erklärt, dass er alle Rechte am beschriebenen Werk besitzt.</p>
-	                        <p class="text-danger accordion">Diese Angaben zur Registeranmeldung werden nicht veröffentlicht! &nbsp;<i class="fa fa-chevron-down" aria-hidden="true"></i></p>
-						</div>
+						<p>Der Urheber ist vollständig fur den Inhalt der Darstellung verantworlich und erklärt, dass er alle Rechte am beschriebenen Werk besitzt.</p>
 					</div>
-					<div class="col-sm-6 col-md-4 col-lg-4 panel">
+					<div class="col-sm-6 col-md-6 col-lg-6 panel">
 						<div class="form-group">							
 							<label for="">Webseite</label>
 							<input type="text" name="webseite" maxlength="150" class="form-control" placeholder="Webseite">							
@@ -92,9 +89,7 @@ get_header();
                         <div class="form-group">							
 							<label for="">Locarno Klassifikation</label>
 							<input type="text" name="locarno" class="form-control" maxlength="50" value="00.00">							
-                        </div>                       
-					</div>
-					<div class="col-sm-6 col-md-4 col-lg-4 panel">
+						</div>   
 						<div class="form-group">							
 							<label for="">Internationale Patentklassifikation</label>
 							<input type="text" name="internationale" class="form-control" maxlength="50" value="00.00">							
@@ -102,13 +97,13 @@ get_header();
 						<div class="form-group">							
 							<label for="">Nizzaklassifikation</label>
 							<input type="text" name="nizzaklassifikation" class="form-control" maxlength="50" value="00.00">							
-						</div>
+						</div>                    
+					</div>
+					<div class="col-sm-6 col-md-6 col-lg-6 panel">						
 						<div class="form-group">
                             <label for="">Keword Nr 1 </label>
                             <input type="text" name="keywordnr1" maxlength="40" class="form-control keyword1" placeholder="Keword Nr 1" value="Stichwort / Schlagwort">
-                        </div>                        
-					</div>
-					<div class="col-sm-6 col-md-4 col-lg-4 panel">
+                        </div>
 						<div class="form-group">
                             <label for="">Keword Nr 2 </label>
                             <input type="text" name="keywordnr2" maxlength="40" class="form-control keyword2"  placeholder="Keyword Nr 2" value="Stichwort / Schlagwort">
@@ -125,6 +120,12 @@ get_header();
                             <label for="">Keword Nr 5 </label>
                             <input type="text" name="keywordnr5" maxlength="40" class="form-control keyword5"  placeholder="Keword Nr 5"  value="Stichwort / Schlagwort">
                         </div>
+					</div>
+					<div class="col-md-12">
+						<div class="form-group">	                        
+							<p class="text-danger">Diese Angaben zur Registeranmeldung werden nicht veröffentlicht! &nbsp;</p>
+							<p class="accordion">Optionale Bildbeschreibung <i class="fa fa-chevron-down" aria-hidden="true"></i></p>
+						</div>
 					</div>
 				</div>
 				<div class="row">
