bab394a8cb2dbefa51656aec76c07f75ce194e51
sibbir@anysense.co.jp <sibbir@anysense.co.jp>
2020-12-05 21:46:11 +0600

    Update home page

--- footer.php
+++ footer.php
@@ -10,42 +10,48 @@
  */
 
 ?>
-<div class="container-fluid bg-color footer">
-	<div class="container">
-		<div class="row p-5">
-			<div class="col-md-4 col-sm-4">
-				<?php
-				if(is_active_sidebar('left-footer')) {
-					dynamic_sidebar('left-footer');
-				}
-				?>
-			</div>	
-			<div class="col-md-4 col-sm-4">
-				<?php
-				if(is_active_sidebar('middle-footer')) {
-					dynamic_sidebar('middle-footer');
-				}
-				?>
-			</div>
-			<div class="col-md-4 col-sm-4">
-				<?php
-				if(is_active_sidebar('right-footer')) {
-					dynamic_sidebar('right-footer');
-				}
-				?>
+<footer>
+	<div class="container-fluid footer">
+		<div class="container">
+			<div class="row p-5">
+				<div class="col-md-4 col-sm-4">
+					<?php
+					if(is_active_sidebar('left-footer')) {
+						dynamic_sidebar('left-footer');
+					}
+					?>
+				</div>	
+				<div class="col-md-4 col-sm-4">
+					<?php
+					if(is_active_sidebar('middle-footer')) {
+						dynamic_sidebar('middle-footer');
+					}
+					?>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<?php
+					if(is_active_sidebar('right-footer')) {
+						dynamic_sidebar('right-footer');
+					}
+					?>
+				</div>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<?php
-				if(is_active_sidebar('copyright')) {
-					dynamic_sidebar('copyright');
-				}
-				?>
+	</div> <!-- container div close -->
+	<div class="container-fluid footer-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<?php
+					if(is_active_sidebar('copyright')) {
+						dynamic_sidebar('copyright');
+					}
+					?>
+				</div>
 			</div>
 		</div>
 	</div>
-</div> <!-- container div close -->
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
--- page-templates/home.php
+++ page-templates/home.php
@@ -10,6 +10,7 @@ get_header();
 ?>
 
 <div class="container main-container">
+
 	<div class="row">
 		<div class="col-md-12 text-center">
 			<h1><?php _e('Common Copyright Register of Intellectual Property Rights', 'ccroipr'); ?></h1>
@@ -39,42 +40,45 @@ get_header();
 					<li>
 						<input id='c5' type='checkbox'>
 						<label for='c5'>D <span class="tooltiptext tooltip-left">Tooltip text</span></label>
-					</li>					
+					</li>				
 				</ul>
 				<button><img src="<?php echo get_template_directory_uri() . '/assets/img/ccroipr-circle-logo-red.png'; ?>" alt=""></button>
 			</div>
 		</div>
 
-		<div class="col-md-6 offset-md-3">
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Search..">
-				<div class="input-group-append">
-					<button class="btn btn-secondary" type="button">
-						<i class="fa fa-search"></i>
+		<div class="col-md-6 col-md-offset-3">
+			<div class="input-group mb-3">
+				<input type="text" class="form-control" placeholder="Search">
+				<div class="input-group-btn">
+					<button class="btn btn-default" type="submit">
+						<i class="glyphicon glyphicon-search"></i>
 					</button>
+				</div>
+			</div>
+			<div class="row text-center mt-3">
+				<div class="col-sm-4">
+					<span>00000</span>
+					<p><b><?php _e('Days Online', 'ccroipr'); ?></b></p>
+				</div>
+				<div class="col-sm-4">
+					<span>000001</span>
+					<p><b><?php _e('Visitors', 'ccroipr'); ?></b></p>
+				</div>
+				<div class="col-sm-4">
+					<span>00007</span>
+					<p><b><?php _e('Uploads', 'ccroipr'); ?></b></p>
 				</div>
 			</div>
 		</div>
 	</div>
-	
-	<div class="row text-center mt-3">
-		<div class="col-md-4">
-			<span>00000</span>
-			<p><b><?php _e('Days Online', 'ccroipr'); ?></b></p>
-		</div>
-		<div class="col-md-4">
-			<span>000001</span>
-			<p><b><?php _e('Visitors', 'ccroipr'); ?></b></p>
-		</div>
-		<div class="col-md-4">
-			<span>00007</span>
-			<p><b><?php _e('Uploads', 'ccroipr'); ?></b></p>
-		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-lg-12 text-center mb-5 mt-5 bg-color p-5">
-			<h2 class="text-light"><a href="https://www.atelier-kalai.de/index.htm#Anker-11"><?php _e('Der Kunstverlag Atelier Kalai Ihr Fachverlag für Rasterzeichnung Vorlagen & Zählmuster', 'ccroipr'); ?></a></h2>
+	<div class="row mt-3">
+		<div class="col-lg-12">
+			<h2 class="text-light text-center bg-color p-3">
+				<a href="https://www.atelier-kalai.de/index.htm#Anker-11">
+					<?php _e('Der Kunstverlag Atelier Kalai Ihr Fachverlag für Rasterzeichnung Vorlagen & Zählmuster', 'ccroipr'); ?>
+				</a>
+			</h2>
 		</div>
 	</div>
 </div>
